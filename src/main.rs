3937ec2fff492c19e49ff9c9bfd4e3af05036b55
azizadx <azizadx.tech@gmail.com>
2023-08-30 19:32:32 +0300

    config Bracket-lib successfully

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+// #[derive(Debug)]
+struct State {
+} 
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello World!")
+    }
+}
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+            .with_title("Flabby Brid")
+            .build()?;
+    main_loop(context, State{})
 }
